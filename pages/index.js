4bf2ecf2f06dec80c4ad08bc8369ea5fafd674b7
n22003 <n22003@std.it-college.ac.jp>
2023-02-14 07:29:01 +0000

    2.3 base

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
+import Header from 'components/header'
+import Hero from 'components/hero'
+import Footer from 'components/footer'
+
 const Home = () => {
   return (
     <>
-      <header>HEADER</header>
+      <Header />
 
       <main>
-        <h1>CUBE</h1>
-        <p>アウトプットしていくサイト</p>
+        <Hero />
       </main>
 
-      <footer>FOOTER</footer>
+      <Footer />
     </>
   )
 }

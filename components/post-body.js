090e6533ab776fb22be8413995af5c6ecf5f6a3d
n22003 <n22003@std.it-college.ac.jp>
2023-02-16 06:43:49 +0000

    4.6 css

--- components/post-body.js
+++ components/post-body.js
@@ -1,5 +1,7 @@
+import styles from 'styles/post-body.module.css'
+
 const PostBoby = ({ children }) => {
   return (
-    <div>{children}</div>)
+    <div className={styles.stack}>{children}</div>)
 }
 export default PostBoby

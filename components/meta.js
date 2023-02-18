9341f2b71d6aa4e437630736abcd5cfd0bbc48ce
n22003 <n22003@std.it-college.ac.jp>
2023-02-18 09:56:34 +0000

    7.6

--- components/meta.js
+++ components/meta.js
@@ -3,13 +3,19 @@ import { useRouter } from 'next/router'
 
 // サイトに関する情報
 import { siteMeta } from 'lib/constants'
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } =
+  siteMeta
 
 // 汎用OGP画像
 import siteImg from 'images/ogp.jpg'
 
-const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } = siteMeta
-
-const Meta = ({ pageTitle, pageDesc, pageImg, pageImgW, pageImgH }) => {
+export default function Meta({
+  pageTitle,
+  pageDesc,
+  pageImg,
+  pageImgW,
+  pageImgH,
+}) {
   // ページのタイトル
   const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
 
@@ -29,26 +35,25 @@ const Meta = ({ pageTitle, pageDesc, pageImg, pageImgW, pageImgH }) => {
   return (
     <Head>
       <title>{title}</title>
-      <meta property='og:title' content={title} />
+      <meta property="og:title" content={title} />
 
-      <meta name='description' content={desc} />
-      <meta property='og:description' content={desc} />
+      <meta name="description" content={desc} />
+      <meta property="og:description" content={desc} />
 
-      <link rel='canonical' href={url} />
-      <meta property='og:url' content={url} />
+      <link rel="canonical" href={url} />
+      <meta property="og:url" content={url} />
 
-      <meta property='og:site_name' content={siteTitle} />
-      <meta property='og:type' content={siteType} />
-      <meta property='og:locale' content={siteLocale} />
+      <meta property="og:site_name" content={siteTitle} />
+      <meta property="og:type" content={siteType} />
+      <meta property="og:locale" content={siteLocale} />
 
-      <link rel='icon' href={siteIcon} />
-      <link rel='apple-touch-icon' href={siteIcon} />
+      <link rel="icon" href={siteIcon} />
+      <link rel="apple-touch-icon" href={siteIcon} />
 
-      <meta property='og:image' content={imgUrl} />
-      <meta property='og:image:width' content={imgW} />
-      <meta property='og:image:height' content={imgH} />
-      <meta name='twitter:card' content='summary_large_image' />
+      <meta property="og:image" content={imgUrl} />
+      <meta property="og:image:width" content={imgW} />
+      <meta property="og:image:height" content={imgH} />
+      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   )
 }
-export default Meta
